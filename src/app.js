c117c9e95127a38b26f4b1f90066ad5aaa45c8ce
harshhere905 <hv96829@gmail.com>
2026-06-03 20:50:09 +0530

    test: add basic server running route

--- src/app.js
+++ src/app.js
@@ -7,6 +7,11 @@ const app=express();
 
 app.use(express.json());
 app.use(cookieParser());
+app.get('/',(req,res)=>{
+    res.status(200).json({
+        message: "Welcome to the API"
+    });
+});
 app.use('/api/v1/auth',authRoutes);
 app.use('/api/v1/account',accountRoutes);
 app.use('/api/v1/transaction',transactionRoutes);
